803ce4d0e7db8c861d3aba058bf59ad3b1d8e43e
zhengshu <zhengshu@mit.edu>
2012-12-04 02:23:14 -0500

    Added 3 test file skeletons with proposed testing strategies

---
+++ proj2/src/gui/guiTest.java
@@ -0,0 +1,20 @@
+
+
+/**
+ * Test Strategy for GUI Test
+ * 
+ * (1) Test JButton "create new file"
+ *     check that a new GUI window will pop up with a correct
+ *     update on the document numbers
+ * (2) Test JTextField "Edit History"
+ *     check that each line in the edit history corresponds to
+ *     its respective edit event
+ * (3) Usability Testing
+ *     make sure that all the interactive JComponents (i.e., drop-
+ *     down box, editable text field, Jbutton, etc) correctly listen
+ *     and respond to action events from the client side
+ * 
+ * 
+*/
+
+
---
+++ proj2/src/model/ModelTest.java
@@ -0,0 +1,15 @@
+
+/**
+* Testing Strategy for the Model
+* 
+* (1) Test that the model can be correctly constructed with any
+*     initial texts in the editable text field
+*     
+* (2) Test that an edit in the view by any client will be reflected
+*     instantaneous in the model
+*
+* (3) Test specific methods in the Model.class
+*     i) getDoc (get document)
+*     ii) getDocName (get the name of the document)
+*
+*/
